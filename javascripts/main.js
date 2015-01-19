1dd45bbf512719149b483c281e4f5467f038708e
Holderness <andersonholderness@gmail.com>
2015-01-19 02:19:26 -0500

    smoothes link animations with .stop()

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,50 +1,58 @@
 $(function(){
   if ($(window).width() < 650) {
     $('.link').hover(function() {
-      $(this).animate({
+      $(this).stop(true, false).animate({
         width: "75px",
+        marginTop: -35,
+        marginLeft: -10
       },200);
     },
     function() {
-      $(this).animate({
+      $(this).stop(true, false).animate({
         width: "60px",
+        marginTop: 0,
+        marginLeft: 0
       },200);
     });
   } else {
     $('.link').hover(function() {
-      $(this).animate({
-        width: "120px",
+      $(this).stop(true, false).animate({
+        width: "130px",
+        marginTop: -50,
+        marginLeft: -20
       },200);
     },
     function(){
-      $(this).animate({
+      $(this).stop(true, false).animate({
         width: "90px",
+        marginTop: 0,
+        marginLeft: 0
       },200);
     });
 	}
   
   if ($(window).width() < 650) {
     $('.pig-image').hover(function() {
-      $('.fly-pig-tongue').filter(':not(:animated)').animate({
+      $('.fly-pig-tongue').stop(true, false).animate({
         marginTop: 0,
         marginLeft: 0
       }, 2000);
     },
     function() {
-      $('.fly-pig-tongue').animate({
+      $('.fly-pig-tongue').stop(true, false).animate({
         marginTop: -9,
         marginLeft: -3
       }, 200);
     });
   } else {
     $('.pig-image').hover(function() {
-      $('.fly-pig-tongue').filter(':not(:animated)').animate({
+      $('.fly-pig-tongue').stop(true, false).animate({
         marginTop: 0,
         marginLeft: 0
       }, 2000);
     },
     function() {
-      $('.fly-pig-tongue').animate({
+      $('.fly-pig-tongue').stop(true, false).animate({
         marginTop: -18,
         marginLeft: -7
       }, 200);
