cb0f0dbc322b9dea461be78a7989ac9fb3983601
Holderness <andersonholderness@gmail.com>
2015-03-03 16:11:58 -0500

    adds github link to all projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -39,7 +39,9 @@ var pigcaveProject = new Project( {
   description: "This app's foundation is a word-sorting algorithm that pairs similar texts together. The data comes from two websites I scraped and is housed in an API built on Rails. The original images were drawn in Photoshop and animated with Skrollr.js.",
   technologies: "Backbone.js, Skrollr.js, jQuery, Ruby on Rails, JSON, Photoshop",
   image: "css/images/pig-cave-project.png",
-  link: "#portfolio/waiter"
+  link: "#portfolio/waiter",
+  github: "https://github.com/Holderness/Pig_Cave_JS_Client",
+  githubIcon: "css/images/github-icon.png"
 });
 
 var waiterProject = new Project( {
@@ -48,7 +50,9 @@ var waiterProject = new Project( {
   description: "The system allows users to keep track of orders, tables, and menu items. The design is minimal and tight for quick interaction on mobile devices.",
   technologies: "Sinatra, jQuery, PostgreSQL",
   image: "css/images/waiter-project.png",
-  link: "#portfolio/hangmangler"
+  link: "#portfolio/hangmangler",
+  github: "https://github.com/Holderness/Waiter",
+  githubIcon: "css/images/github-icon.png"
 });
 
 var hangmanglerProject = new Project({
@@ -57,7 +61,9 @@ var hangmanglerProject = new Project({
   description: "Play hangman or tic-tac-toe in this ASCII art themed gaming app.  It's got talking cows, moving clouds, and existentialism.",
   technologies: "Sinatra, jQuery, PostgreSQL, Bcrypt, AJAX",
   image: "css/images/hangmangler-project.png",
-  link: "#portfolio/pigcave"
+  link: "#portfolio/pigcave",
+  github: "https://github.com/Holderness/Hangmangler-Helper",
+  githubIcon: "css/images/github-icon.png"
 });
 
 
