ce89ebdf855d93b27a05fee2da72906cda2e185b
Holderness <andersonholderness@gmail.com>
2015-06-13 16:46:37 -0400

    syntax fix

--- javascripts/views/writingTextView.js
+++ javascripts/views/writingTextView.js
@@ -1,20 +1,20 @@
-var app = app || {};
+// var app = app || {};
 
-(function($) {
+// (function($) {
 
-  app.WritingTextView = Backbone.View.extend({
-    el: ".writing-text-container",
-    initialize: function(options){
-      debugger;
-      app.vieww = options;
-      this.render(app.vieww);
-    },
-    template: _.template($('script[name=' + options + ']').html()),
-    render: function(options) {
-      debugger;
-      this.$el.empty();
-      this.$el.html(this.template(options)).hide().fadeIn(200);
-    }
-  });
+//   app.WritingTextView = Backbone.View.extend({
+//     el: ".writing-text-container",
+//     initialize: function(options){
+//       debugger;
+//       app.vieww = options;
+//       this.render(app.vieww);
+//     },
+//     template: _.template($('script[name=' + options + ']').html()),
+//     render: function(options) {
+//       debugger;
+//       this.$el.empty();
+//       this.$el.html(this.template(options)).hide().fadeIn(200);
+//     }
+//   });
 
-})(jQuery);
+// })(jQuery);
