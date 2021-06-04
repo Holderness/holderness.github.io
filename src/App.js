36cd6db6fffc736c21dbd55782cca47ea6705f82
holderness <andersonholderness@gmail.com>
2021-06-04 18:51:37 -0400

    added create-react-app, bulma, cname

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+import Main from './components/Main'
+import 'bulma';
+
+function App() {
+  return (
+    <Main></Main>
+  );
+}
+
+export default App;
+
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PigContainer = () => {
+  return (<div className="block pig $orange">Pig</div>)
+};
+
+const Main = () => {
+  return (<div className="container"><PigContainer/></div>);
+};
+
+export default Main;
